21f7c03ee522d29585b6cf7914cbc16a420a2bdf
Laurrencia <elaurrencia27@gmail.com>
2022-06-23 09:48:46 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = "Name: Laurrencia 0. Francis Joseph,"
+let height = "Height: 5ft 11inch,"
+let country = "Country: Nigeria"
+
+document.write(name);
+document.write(height);
+document.write(country);
+
